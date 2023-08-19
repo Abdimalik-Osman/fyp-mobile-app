72b761aebd11e57b31d1dec8c1c89aa029ad8709
Abdimalik-Osman <engabdimalik8818@gmail.com>
2023-08-19 16:26:29 +0300

    some changes made

--- passportapp/lib/main.dart
+++ passportapp/lib/main.dart
@@ -10,103 +10,145 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'SOMALIA ONLINE PASSPORT APP',
-      theme: ThemeData(primarySwatch: Colors.blue),
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: MaterialColor(0xFF164E63, <int, Color>{
+          50: Color(0xFF164E63),
+          100: Color(0xFF164E63),
+          200: Color(0xFF164E63),
+          300: Color(0xFF164E63),
+          400: Color(0xFF164E63),
+          500: Color(0xFF164E63),
+          600: Color(0xFF164E63),
+          700: Color(0xFF164E63),
+          800: Color(0xFF164E63),
+          900: Color(0xFF164E63),
+        }),
+      ),
       home: HomeScreen(),
     );
   }
 }
 
 class HomeScreen extends StatelessWidget {
+  final cyan900Color = Color.fromRGBO(22, 78, 99, 1);
+  final cyan700Color1 = Color.fromRGBO(14, 116, 144, 1);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('SOMALIA ONLINE PASSPORT APP')),
-      drawer: Drawer(
-        child: ListView(
-          padding: EdgeInsets.zero,
-          children: <Widget>[
-            DrawerHeader(
-              decoration: BoxDecoration(
-                color: Colors.blue,
-              ),
-              child: Text(
-                'Menu',
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 24,
+        appBar: AppBar(title: Text('SOMALIA ONLINE PASSPORT APP')),
+        drawer: Drawer(
+          backgroundColor: cyan700Color1,
+          child: ListView(
+            padding: EdgeInsets.zero,
+            children: <Widget>[
+              DrawerHeader(
+                decoration: BoxDecoration(
+                  color: cyan900Color,
+                ),
+                child: Text(
+                  'Menu',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 24,
+                  ),
                 ),
               ),
-            ),
-            ListTile(
-              leading: Icon(Icons.home),
-              title: Text('Apply'),
-              onTap: () {
-                Navigator.pop(context); // Closes the drawer
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) =>
-                        WebViewPage(url: 'https://sopas.vercel.app/'),
-                  ),
-                );
-              },
-            ),
-            ListTile(
-              leading: Icon(Icons.abc),
-              title: Text('View Applicant'),
-              onTap: () {
-                Navigator.pop(context); // Closes the drawer
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) =>
-                        WebViewPage(url: 'https://sopas.vercel.app/view'),
-                  ),
-                );
-              },
-            ),
-            ListTile(
-              leading: Icon(Icons.message),
-              title: Text('Send Message'),
-              onTap: () {
-                Navigator.pop(context); // Closes the drawer
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) =>
-                        WebViewPage(url: 'https://sopas.vercel.app/send'),
-                  ),
-                );
-              },
-            ),
-            ListTile(
-              leading: Icon(Icons.edit),
-              title: Text('Update Appointment'),
-              onTap: () {
-                Navigator.pop(context); // Closes the drawer
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) =>
-                        WebViewPage(url: 'https://sopas.vercel.app/update'),
-                  ),
-                );
-              },
-            ),
-            ListTile(
-              leading: Icon(Icons.exit_to_app),
-              title: Text('Exit'),
-              onTap: () {
-                Navigator.pop(context); // Closes the drawer
-              },
-            ),
-          ],
+              ListTile(
+                leading: Icon(Icons.person_add),
+                title: Text('Apply'),
+                textColor: Colors.white,
+                iconColor: Colors.white,
+                onTap: () {
+                  Navigator.pop(context); // Closes the drawer
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) =>
+                          WebViewPage(url: 'https://sopas.vercel.app/'),
+                    ),
+                  );
+                },
+              ),
+              ListTile(
+                textColor: Colors.white,
+                iconColor: Colors.white,
+                leading: Icon(Icons.visibility),
+                title: Text('View Applicant'),
+                onTap: () {
+                  Navigator.pop(context); // Closes the drawer
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) =>
+                          WebViewPage(url: 'https://sopas.vercel.app/view'),
+                    ),
+                  );
+                },
+              ),
+              ListTile(
+                textColor: Colors.white,
+                iconColor: Colors.white,
+                leading: Icon(Icons.message),
+                title: Text('Send Message'),
+                onTap: () {
+                  Navigator.pop(context); // Closes the drawer
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) =>
+                          WebViewPage(url: 'https://sopas.vercel.app/send'),
+                    ),
+                  );
+                },
+              ),
+              ListTile(
+                textColor: Colors.white,
+                iconColor: Colors.white,
+                leading: Icon(Icons.edit),
+                title: Text('Update Appointment'),
+                onTap: () {
+                  Navigator.pop(context); // Closes the drawer
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) =>
+                          WebViewPage(url: 'https://sopas.vercel.app/update'),
+                    ),
+                  );
+                },
+              ),
+              ListTile(
+                textColor: Colors.white,
+                iconColor: Colors.white,
+                leading: Icon(Icons.exit_to_app),
+                title: Text('Exit'),
+                onTap: () {
+                  Navigator.pop(context); // Closes the drawer
+                },
+              ),
+            ],
+          ),
         ),
-      ),
-      body: Center(
-        child: Text('WELCOME TO SOMALI ONLINE PASSPORT APPLICATION'),
-      ),
-    );
+        body: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(0),
+            child: Column(
+              children: [
+                Image.asset(
+                  'images/Somalia (1).jpg',
+                  width: double.infinity,
+                  height: 765,
+                  fit: BoxFit.fill,
+                )
+                // Container(
+                //  chil
+                // ),
+              ],
+            ),
+          ),
+        ));
   }
 }
 
@@ -118,7 +160,7 @@ class WebViewPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Web View')),
+      appBar: AppBar(title: Text('')),
       body: WebView(
         initialUrl: url,
         javascriptMode: JavascriptMode.unrestricted,
